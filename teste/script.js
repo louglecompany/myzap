48cb82105a813374d514782f932c93d25b6dc851
louglecompany <144390491+louglecompany@users.noreply.github.com>
2023-12-22 00:45:02 -0300

    Update script.js

--- teste/script.js
+++ teste/script.js
@@ -89,45 +89,125 @@ document.addEventListener('DOMContentLoaded', () => {
         const messageElement = document.createElement('div');
         messageElement.className = 'message';
         messageElement.textContent = `${message.user}: ${message.text}`;
-        messagesContainer.appendChild(messageElement);
-      });
+document.addEventListener('DOMContentLoaded', () => {
+    const loginContainer = document.getElementById('login-container');
+    const chatContainer = document.getElementById('chat-container');
+    const userList = document.getElementById('user-list');
+    const chatHeader = document.getElementById('chat-header');
+    const messagesContainer = document.getElementById('messages');
+    const messageInput = document.getElementById('message-input');
+    const usernameInput = document.getElementById('username-input');
+    const passwordInput = document.getElementById('password-input');
 
+    let selectedUser = null;
+    let currentUser = null;
 
+    // Simulando usuários e senhas
+    const users = [
+        { username: 'Tiago', password: 'tiago123' },
+        { username: 'Jessica', password: 'jessica123' },
+        { username: 'Ana', password: 'ana123' }
+    ];
 
-        document.addEventListener('DOMContentLoaded', () => {
-  const loginForm = document.getElementById('login-form');
-  const chatContainer = document.querySelector('.chat-container');
-  const usernameInput = document.getElementById('username');
-  const passwordInput = document.getElementById('password');
+    // Adicionando eventos de clique para usuários
+    users.forEach(user => {
+        const userElement = document.querySelector(`[data-userid="${user.username}"]`);
+        userElement.addEventListener('click', () => {
+            selectedUser = user.username;
+            chatHeader.textContent = `Conversando com ${user.username}`;
+            renderMessages();
+        });
 
-  const users = [
-    { username: 'tiago', password: 'senha123' },
-    { username: 'fabiana', password: 'senha456' },
-    { username: 'renata', password: 'senha789' }
-    // Adiciona mais usuários com nome de usuário e senha
-  ];
+    // Função para efetuar login
+    window.login = function () {
+        const username = usernameInput.value.trim();
+        const password = passwordInput.value.trim();
 
-  loginForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const enteredUsername = usernameInput.value.toLowerCase();
-    const enteredPassword = passwordInput.value;
+        // Verificando se o usuário e senha correspondem
+        const user = users.find(u => u.username === username && u.password === password);
 
-    const foundUser = users.find(user => user.username === enteredUsername && user.password === enteredPassword);
+        if (user) {
+            currentUser = username;
+            loginContainer.style.display = 'none';
+            chatContainer.style.display = 'block';
+        } else {
+            alert('Usuário ou senha incorretos. Tente novamente.');
+        }
+    };
 
-    if (foundUser) {
-      // Login bem-sucedido, mostra a área do chat e oculta o formulário de login
-      chatContainer.style.display = 'block';
-      document.querySelector('.login-container').style.display = 'none';
-      // Aqui podes adicionar lógica adicional para exibir o chat para o usuário logado
-    } else {
-      alert('Nome de usuário ou senha incorretos. Tente novamente.');
-    }
-  });
+    // Restante do código permanece inalterado
+    // ...
 });
 
+  
+// Função para enviar mensagem
+window.sendMessage = function () {
+    const messageText = messageInput.value.trim();
+    if (messageText !== '' && selectedUser) {
+        const message = { user: currentUser, text: messageText };
+
+        // Armazenar a mensagem no localStorage
+        const storedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+        storedMessages.push(message);
+        localStorage.setItem('chatMessages', JSON.stringify(storedMessages));
+
+        // Limpar o campo de entrada
+        messageInput.value = '';
+
+        // Atualizar a exibição das mensagens apenas se o usuário selecionado for o destinatário
+        if (selectedUser === currentUser) {
+            renderMessages();
+        }
     }
+};
+
   
-    // Renderizar mensagens quando a página carregar
-    renderMessages();
-  });
-  
+// Função para enviar mensagem
+window.sendMessage = function () {
+    const messageText = messageInput.value.trim();
+    if (messageText !== '' && selectedUser) {
+        const message = { sender: currentUser, receiver: selectedUser, text: messageText };
+
+        // Armazenar a mensagem no localStorage
+        const storedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+        storedMessages.push(message);
+        localStorage.setItem('chatMessages', JSON.stringify(storedMessages));
+
+        // Limpar o campo de entrada
+        messageInput.value = '';
+
+        // Atualizar a exibição das mensagens apenas se o usuário selecionado for o destinatário
+        if (selectedUser === currentUser) {
+            renderMessages();
+        }
+    }
+};
+
+// Função para renderizar as mensagens
+function renderMessages() {
+    messagesContainer.innerHTML = '';
+
+    // Obter mensagens do localStorage
+    const storedMessages = JSON.parse(localStorage.getItem('chatMessages')) || [];
+
+    // Filtrar mensagens para exibir apenas as mensagens entre o usuário logado e o usuário selecionado
+    const userMessages = storedMessages.filter(message =>
+        (message.sender === currentUser && message.receiver === selectedUser) ||
+        (message.sender === selectedUser && message.receiver === currentUser)
+    );
+
+    // Adicionar mensagens ao contêiner
+    userMessages.forEach(message => {
+        const messageElement = document.createElement('div');
+        messageElement.className = 'message';
+        messageElement.textContent = `${message.sender}: ${message.text}`;
+        messagesContainer.appendChild(messageElement);
+    });
+}
+
+// Restante do código permanece inalterado
+// ...
+
+// Renderizar mensagens quando a página carregar
+renderMessages();
+});
