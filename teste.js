fd8e54a819bd27b8bf5fa54b8767a3667b3b7ffa
louglecompany <144390491+louglecompany@users.noreply.github.com>
2024-01-02 14:49:15 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,14 @@
+// Arquivo1.js
+var Arquivo1 = {
+    iniciar: function() {
+        // Código do Arquivo1
+        
+    }
+};
+
+// Arquivo2.js
+var Arquivo2 = {
+    iniciar: function() {
+        // Código do Arquivo2
+    }
+};
